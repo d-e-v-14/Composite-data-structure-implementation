7eb113bc69cd5867964df7a5b5ee5ad18e6c48c2
d-e-v-14 <mehandrudev@gmail.com>
2025-03-04 18:10:40 +0530

    addNode and addInterval function created

--- main.c
+++ main.c
@@ -1,15 +1,59 @@
 #include<stdio.h>
 #include<stdlib.h>
+#define MAX_LENGTH 100
 
-struct intervals{
-  int start;
-  int end;
+typedef struct{
+    int start;
+    int end;
+    
+}interval;
+
+struct Node{
+    interval *i;
+    int max;
+    struct Node *left;
+    struct Node *right;
   
 };
 
 
+struct Node *createNode(int start, int end){
+    struct Node *node = (struct Node *)malloc(sizeof(struct Node));
+    node->i=(interval *)malloc(sizeof(interval));
+    node->i->start=start;
+    node->i->start=end;
+    node->max=end;
+    node->left=NULL;
+    node->right=NULL;
+    
+    return node;
+}
 
+struct Node *addIntervals(struct Node *head,int start,int end){
+    
+    if(head==NULL){
+        return createNode(start,end);
+    }
+
+    if(head->i->start>start){
+        head->right=addIntervals(head->right,start,end);
+    }else{
+        head->left=addIntervals(head->left,start,end);
+    }
+    
+    if(head->max<end){
+        head->max=end;
+    }
+    
+    return head;
+
+}
 
 int main(){
+    struct Node *head=NULL;
     
+    
+    head = addIntervals(head,1,3);
+    
+    return 0;
 }
