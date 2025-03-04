11fb4d643edda2036ae035c0b762b58a49834b80
d-e-v-14 <mehandrudev@gmail.com>
2025-03-04 18:28:32 +0530

    added a function to print tree without merging

--- main.c
+++ main.c
@@ -21,7 +21,7 @@ struct Node *createNode(int start, int end){
     struct Node *node = (struct Node *)malloc(sizeof(struct Node));
     node->i=(interval *)malloc(sizeof(interval));
     node->i->start=start;
-    node->i->start=end;
+    node->i->end=end;
     node->max=end;
     node->left=NULL;
     node->right=NULL;
@@ -36,9 +36,9 @@ struct Node *addIntervals(struct Node *head,int start,int end){
     }
 
     if(head->i->start>start){
-        head->right=addIntervals(head->right,start,end);
-    }else{
         head->left=addIntervals(head->left,start,end);
+    }else{
+        head->right=addIntervals(head->right,start,end);
     }
     
     if(head->max<end){
@@ -49,11 +49,35 @@ struct Node *addIntervals(struct Node *head,int start,int end){
 
 }
 
+void printtree(struct Node *head){
+    if(head==NULL){
+        printf("---<empty>---\n");
+        return;
+    }
+    
+    printf("start: %d\n",(head->i->start));
+    printf("end: %d\n",(head->i->end));
+    
+    printf("--left--\n");
+    printtree(head->left);
+    
+    printf("--right--\n");
+    printtree(head->right);
+    
+    
+}
+
+
 int main(){
     struct Node *head=NULL;
     
     
     head = addIntervals(head,1,3);
+    head = addIntervals(head,2,6);
+    head = addIntervals(head,4,5);
+    head = addIntervals(head,3,10);
+    
+    printtree(head);
     
     return 0;
 }
